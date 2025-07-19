ba5dd9d54b4eb9a65ebcff9f2551fc8bf0a5181f
BaskaranV15 <baskaranv1315@gmail.com>
2025-07-19 11:06:00 +0530

    initial commit:

---
+++ script.js
@@ -0,0 +1,15 @@
+function createCounter() {
+  let count = 0;
+
+  return function () {
+    count++;
+    return count;
+  };
+}
+const counter = createCounter();
+
+function handleClick() {
+  const resultDiv = document.getElementById("result");
+  const currentCount = counter();
+  resultDiv.textContent = `Count: ${currentCount}`;
+}
